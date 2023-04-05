941b5e65594c74243c8e8d8f0d254613e383448a
Dav0101 <129580907+Dav0101@users.noreply.github.com>
2023-04-05 15:13:06 +0200

    Update PeriodNaive.c

--- Periodo Frazionario C/PeriodNaive.c
+++ Periodo Frazionario C/PeriodNaive.c
@@ -1,10 +1,9 @@
 int PeriodNaive (char *X, int size) {       //fare partire X da 1
     int p,j;
     for (p=1; p<=size; p++) {
-        for (j=p+1; j<=size; j++) {
-            if ( X[j]!=X[j-p]) break;
+        for (j=p+1; j<=size && X[j]==X[j-p]; j++) {
+            if ( j==size ) return p;
         }
-        if (j==size) return p;
     }
-    return p;
+    return p-1;
 }
